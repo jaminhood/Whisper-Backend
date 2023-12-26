e41de66e79e452705ece0890460702041fb92e2e
jaminhoood <princeobj5@gmail.com>
2023-12-26 17:23:50 +0100

    feat: cors origin updated

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5500", "http://localhost:8080"]
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5500", "http://localhost:8080", "http://127.0.0.1:8080"]
 
 module.exports = allowedOrigins

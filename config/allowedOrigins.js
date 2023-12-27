b287674823e03941999561f9d4354696db500e1d
Benjamin Ovu <31051845+jaminhood@users.noreply.github.com>
2023-12-27 20:06:01 +0100

    Update allowedOrigins.js

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,10 @@
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5500", "http://127.0.0.1:5500", "http://localhost:8080", "http://127.0.0.1:8080"]
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5500",
+  "http://127.0.0.1:5500",
+  "http://localhost:8080",
+  "http://127.0.0.1:8080",
+  "https://whisper-app-three.vercel.app/"
+]
 
 module.exports = allowedOrigins
